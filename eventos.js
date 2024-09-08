a4ce03837b906f1888671708af952bca20980337
eyarualanda <eyarualanda@gmail.com>
2024-09-08 15:47:44 -0300

    SOlución al click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const elDiv = document.getElementById('el-div');
+    const elButton = document.getElementById('button');
     elDiv.addEventListener('click', () => {
         alert('Hola! Soy el div');
+    });     
+    elButton.addEventListener('click', (event) => {
+        event.stopPropagation(); 
     });
 });
+
