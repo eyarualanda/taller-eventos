d11eb21addaec6f90931f507fc163cf3b3dce4e4
eyarualanda <eyarualanda@gmail.com>
2024-09-08 13:27:11 -0300

    "SItio web creado"

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const elDiv = document.getElementById('el-div');
+    elDiv.addEventListener('click', () => {
+        alert('Hola! Soy el div');
+    });
+});
